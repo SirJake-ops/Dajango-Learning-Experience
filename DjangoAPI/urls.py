1e623ca58bffa1d9b9fc1ae63d486693b747771e
Jacob Pagan <jacob.pagan10@gmail.com>
2025-03-10 18:01:42 -0500

    ...

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from application_user.views import ApplicationUserLoginView
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('application_user.urls')),
     path('api/bugs/', include('tracker.urls')),
-    path('api/token/',ApplicationUserLoginView .as_view(), name='token_obtain_pair'),
+    path('api/token/', ApplicationUserLoginView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
--- application_user/views.py
+++ application_user/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import ApplicationUser
@@ -7,6 +8,7 @@ from .serializers import ApplicationUserSerializer
 
 
 class ApplicationUserListView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated)
     queryset = ApplicationUser.objects.all()
     serializer_class = ApplicationUserSerializer
 
